baf9d1d51b2022b6964cea6f69029778fd7e3866
ArnBon <bonillaarnaldo@gmail.com>
2024-12-03 19:07:46 -0400

    fin clase 44 enviando parametros por URL

--- src/app/pages/item/item.component.ts
+++ src/app/pages/item/item.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
 
 @Component({
   selector: 'app-item',
@@ -7,6 +8,19 @@ import { Component } from '@angular/core';
   templateUrl: './item.component.html',
   styleUrl: './item.component.scss'
 })
-export class ItemComponent {
+export class ItemComponent implements OnInit{
+
+  constructor(private route: ActivatedRoute){}
+
+
+  ngOnInit(): void {
+    /*se vana a pasar los parametros
+    de la url es decir para saber que id es
+    es decir el parametro es el id */
+    this.route.params
+    .subscribe(parametros =>{
+      console.log(parametros)
+    })
+  }
 
 }
